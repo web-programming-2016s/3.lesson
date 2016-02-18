9c3183fb22f20ad61584293eb1f3237522078e46
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-02-18 11:36:25 +0200

    added new field

--- app.php
+++ app.php
@@ -1,14 +1,24 @@
 <?php
 	
+	//Getting the message from address
+	// if there is ?name= .. then $_GET["name"]
+	$my_message = $_GET["message"];
+	$to = $_GET["to"];
 	
+	echo "My message is ".$my_message." and is to ".$to;
 
 ?>
 
 <h2> First application </h2>
 
 <form method="get">
+	<label for="to">to:* <label>
+	<input type="text" name="to"><br><br>
+	
 	<label for="message">Message:* <label>
 	<input type="text" name="message"><br><br>
+	
+	<!-- This is the save button-->
 	<input type="submit" value="Save to DB">
 
 <form>
