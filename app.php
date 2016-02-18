e98f5ed36b0932c0eca4ca1df8ba6f3f9ce7edc4
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-02-18 12:30:47 +0200

    adde form validation

--- app.php
+++ app.php
@@ -1,11 +1,32 @@
 <?php
+
+	//check if there is variable in the URL
+	if(isset($_GET["message"])){
+		
+		//only if there is message in the URL
+		//echo "there is message";
+		
+		//if its empty
+		if(empty($_GET["message"])){
+			//it is empty
+			echo "Please enter the message!";
+		}else{
+			//its not empty
+			echo "Message: ".$_GET["message"]."<br>";
+		}
+		
+	}else{
+		//echo "there is no such thing as message";
+	}
 	
 	//Getting the message from address
 	// if there is ?name= .. then $_GET["name"]
-	$my_message = $_GET["message"];
-	$to = $_GET["to"];
+	//$my_message = $_GET["message"];
+	//$to = $_GET["to"];
 	
-	echo "My message is ".$my_message." and is to ".$to;
+	
+	//echo "My message is ".$my_message." and is to ".$to;
+	
 
 ?>
 
