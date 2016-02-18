8c9e9e488c1dd93aba645312bbeb8ae499f52b9d
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-02-18 10:57:27 +0200

    added app

--- app.php
+++ app.php
@@ -1,1 +1,14 @@
-APP
+<?php
+	
+	
+
+?>
+
+<h2> First application </h2>
+
+<form method="get">
+	<label for="message">Message:* <label>
+	<input type="text" name="message"><br><br>
+	<input type="submit" value="Save to DB">
+
+<form>
